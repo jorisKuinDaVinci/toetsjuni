a3708ad2f32b5c9493367034967d8cfe1545a9b6
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-06-21 10:04:43 +0200

    bijgewerkt

--- python/hoe_vaak_tanken.py
+++ python/hoe_vaak_tanken.py
@@ -1,10 +1,3 @@
-#Je bent eruit. Nadat je thuis bent gekomen ga je op vakantie naar Zuid-Spanje.
-#Hoe vaak moet je tanken om op je bestemming te komen? 
-#Je vertrekt met een volle tank, je rijdt de tank precies leeg en vult deze dan volledig. (je kunt dus overal tanken!)
-
-#Schrijf een functie in Python die een integer teruggeeft met het aantal tankbeurten.
-#De functie heeft als parameters: kilometers_per_liter, tankinhoud, te_rijden_afstand.
-#Schrijf minimaal 10 testen voor de functie!
 def hoe_vaak_tanken():
     kilometers_per_liter = 12
     tankinhoud = 60
