293b144927c666021eef0b45eb1fd46fab3488ec
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-06-21 11:32:58 +0200

    bijgewerkt

--- sql/mijn_code.py
+++ sql/mijn_code.py
@@ -1,11 +1,15 @@
-from get_all_cars import *
-from get_destination_by_name import *
-from travel_db import *
+from get_all_cars import get_all_cars
+from get_destination_by_name import get_destination
+
 def list_sahara_cars(destination_name: str) -> list:
     cars = []
     # code om de afstand op te zoeken in de tabel destinations
- 
+    destination = get_destination(destination_name)
+    distance = destination['distance']
     # code om de cars te selecteren die de afstand in één keer kunnen overbruggen
+    for car in get_all_cars():
+        if car['max_distance'] >= distance:
+            cars.append(car)
  
     return cars
  
